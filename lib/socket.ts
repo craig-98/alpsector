eecde2500a72d22bd9cee53921e16c5d17662269
Rufai Ahmed <abbeyrufai234@gmail.com>
2026-03-28 17:40:45 +0100

    fix: backend

--- lib/socket.ts
+++ lib/socket.ts
@@ -2,7 +2,7 @@ import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
 
-export const SOCKET_URL = "http://localhost:4000";
+export const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000";
 
 export const initSocket = (authToken?: string) => {
   if (!socket) {
